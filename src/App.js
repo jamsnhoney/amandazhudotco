34214090f23eb452595194a6f763c742a8731e0a
amanda <116103667+jamsnhoney@users.noreply.github.com>
2025-10-16 20:38:09 -0400

    alskdjgklasdf

--- src/App.js
+++ src/App.js
@@ -2,13 +2,13 @@ import './App.css';
 
 // pages
 import Navbar from './pages/Navbar';
-import Hero from './pages/Navbar';
 import Simple from './pages/Simple';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <Navbar />
         <Simple />
       </header>
     </div>
